6bba774590463b716476b1a1e20c35289323a5a5
Janes Roberto <kadoshmt2@gmail.com>
2020-09-14 14:03:40 -0400

    fix: remove a lib history

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import ReactGA from 'react-ga';
-// import { createBrowserHistory } from 'history';
-
 import AppProvider from './hooks';
-
 import Routes from './routes';
-
-// const history = createBrowserHistory();
 
 const trackingId = 'UA-177944018-1';
 
 ReactGA.initialize(trackingId);
 ReactGA.pageview(window.location.pathname + window.location.search);
-
-// Initialize google analytics page view tracking
-// history.listen(browser => {
-//   ReactGA.set({ page: browser.location.pathname }); // Update the user's current page
-//   ReactGA.pageview(browser.location.pathname); // Record a pageview for the given page
-// });
 
 const App: React.FC = () => {
   return (
